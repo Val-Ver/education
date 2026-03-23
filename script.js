d6ab08cf63400ddf532e0eee9d9f5ea466a0b125
Alina <alisha.vero.mob@gmail.com>
2026-03-23 21:34:54 +0700

    скрытие формы, добавление пустой статьи

--- script.js
+++ script.js
@@ -1,33 +1,82 @@
 window.addEventListener("load", main);
 
+const modal = document.getElementById("modal-statistics");
+
+let closeModal = null;
 
 
 function main() {
-    addListenerBtnAdminPanel();
-    addListenerBtnCloseAdminPanel()
+    addListenerBtnShowStatistics();
+    addListenerBtnShowCreateArticle();
+
+
+}
+
+function addListenerBtnShowStatistics() {
+    const showStatsBtn = document.getElementById("show-stats-btn");
+    const showViewStats = () => {
+        modal.showModal();
+        showCountArticles();
+        addListenerBtnCloseModal();
+    }
+    showStatsBtn.addEventListener("click", showViewStats);
 }
 
 function showCountArticles() {
     const articles = document.querySelectorAll("article");
-
     const counterArticles = document.getElementById("articles-counter");
     counterArticles.textContent = `${articles.length}`;
 }
 
-function addListenerBtnAdminPanel() {
-    const showStatsBtn = document.getElementById("show-stats-btn");
-    const viewStats = document.getElementById("statistics");
-    const showViewStats = () => {
-        viewStats.showModal();
-        showCountArticles();
-    }
-    showStatsBtn.addEventListener("click", showViewStats);
-}
-function addListenerBtnCloseAdminPanel() {
-    const viewStats = document.getElementById("statistics");
+function addListenerBtnCloseModal() {
     const btnCloseDialog = document.getElementById("close-dialog");
-    const closeViewStats = () => {
-        viewStats.close();
+    closeModal = () => {
+        modal.close();
+        btnCloseDialog.removeEventListener("click", closeModal);
     }
-    btnCloseDialog.addEventListener("click", closeViewStats);
+    btnCloseDialog.addEventListener("click", closeModal);
+}
+
+
+
+const form = document.getElementById("form");
+
+function addListenerBtnShowCreateArticle() {
+    const createPostBtn = document.getElementById("create-post-btn");
+    const createArticle = () => {
+        form.removeAttribute('hidden');
+        addListenerBtnCloseForm();
+        addListenerBtnAddArticle();
+    }
+    createPostBtn.addEventListener("click", createArticle);
+}
+
+function addListenerBtnCloseForm() {
+    btnCancel = document.getElementById("cancel");
+
+    const closeForm = () => {
+        form.setAttribute('hidden', '');
+        btnCancel.removeEventListener("click", closeForm);
+    }
+    btnCancel.addEventListener("click", closeForm);
+}
+
+function addListenerBtnAddArticle() {
+    const btnAddArticle = document.getElementById('submit');
+
+    const addArticle = () => {
+        addArticleFromTemtlate();
+        form.setAttribute('hidden', '');
+        btnAddArticle.removeEventListener("click", addArticle);
+    }
+
+    btnAddArticle.addEventListener("click", addArticle);
+}
+
+function addArticleFromTemtlate() {
+    const template = document.getElementById('article-template');
+    const clone = template.content.cloneNode(true);
+
+    const container = document.getElementById('articles-container');
+    container.appendChild(clone);
 }
