250792d430a85b9d6e72196080f6e84a48ebe0e2
Alina <alisha.vero.mob@gmail.com>
2026-04-06 14:17:10 +0700

    first-page

---
+++ blog-app/src/app/ui/pages/main-first-page/main-first-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-first-page',
+  // imports: [],
+  templateUrl: './main-first-page.html',
+  styleUrl: './main-first-page.scss',
+})
+export class MainFirstPage {}
