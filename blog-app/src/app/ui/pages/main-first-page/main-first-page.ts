f32123f0e8b8fa0ff2c40714333d906f2e45785b
Alina <alisha.vero.mob@gmail.com>
2026-04-10 00:34:00 +0700

    стили ссылок

--- blog-app/src/app/ui/pages/main-first-page/main-first-page.ts
+++ blog-app/src/app/ui/pages/main-first-page/main-first-page.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ArticleModel } from '../../../models/article.model'
 import { ArticlesService } from '../../../services/articles.service'
 import { ArticleCard } from '../../components/article-card/article-card'
 
 @Component({
   selector: 'app-main-first-page',
-  imports: [ArticleCard],
+  imports: [RouterLink, ArticleCard],
   templateUrl: './main-first-page.html',
   styleUrl: './main-first-page.scss',
 })
@@ -15,6 +16,6 @@ export class MainFirstPage {
 
   ngOnInit(): void {
     const articlesAll = this.articlesService.getArticles();
-    this.previewArticles = articlesAll.slice( -3).reverse();
+    this.previewArticles = articlesAll.slice(-3).reverse();
   }
 }
